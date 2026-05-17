3caf0cd3b52bc46b16499dcf0418226a52ee526f
Theotime <hamardtheotime@gmail.com>
2026-05-17 19:53:32 +0200

    amélioration de la stratégie de l'ia

--- ia.py
+++ ia.py
@@ -19,26 +19,59 @@ class ktourStrategy:
              defausse # ce qui est déjà joué comme cartes
             ):
         
-        dangers_dangereux = 0
+        # calcule la proba des dangers
+        risque = 0
         for p in pieges:
-            if p in defausse and p in tas_tri:
-                dangers_dangereux += 1
+            if p in defausse:
+                nb_dans_tas = tas_tri.count(p)
+                risque += nb_dans_tas / len(tas_tri)
 
-        if id_manche <= 2:
-            seuil = 3
-        elif id_manche <= 4:
-            seuil = 2
-        else:
-            seuil = 1
+        seuil = 0.3
 
+        # calcule de la position par rapore a la moyenne de tous les joueur
         moyenne_coffre = sum(sum(j["coffre"]) for j in les_joueurs) / len(les_joueurs)
         mon_total = sum(mon_coffre) + mon_sac
         
         if mon_total < moyenne_coffre:
-            seuil += 1
+            seuil += 0.1
         elif mon_total > moyenne_coffre:
-            seuil -= 1
+            seuil -= 0.1
+        
+        if mon_sac > moyenne_coffre:
+            seuil -= 0.1
 
-        seuil = max(1, seuil)
+        nb_actifs = sum(1 for j in les_joueurs if j["is_active"])
+        gain_reel = rubis_au_sol // nb_actifs
 
-        return dangers_dangereux < seuil
+        # calcul en fonction des rubis au sol
+        if gain_reel > moyenne_coffre:
+            seuil -= 0.1
+
+        relique_disponible = any(c.startswith("R_") for c in defausse)
+        seul_actif = nb_actifs == 1
+
+        #calcule en fonction des reliques
+        if relique_disponible:
+            valeur_relique = sum(int(c.split("_")[1]) for c in defausse if c.startswith("R_"))
+
+            # si on est seul et que les reliques vale plus que la motié du sac on rentre 
+            if seul_actif and valeur_relique > mon_sac * 0.5:
+                return False
+            
+            # mm si on est pas seul mais que les relique vale le coup on baisse le seuil
+            if valeur_relique > moyenne_coffre:
+                seuil -= 0.1
+
+        # calcul gain espere
+        somme_tresors_tas = sum(int(c) for c in tas_tri if c.isdigit())
+
+        gain_espere = somme_tresors_tas / nb_actifs
+        if gain_espere > mon_sac:
+            seuil += 0.1
+        elif gain_espere < mon_sac:
+            seuil -= 0.1
+
+
+        seuil = max(0.1, seuil) # bloque le seuil à 0.1
+
+        return risque < seuil
